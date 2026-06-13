26b0d404870116dfef42d806edee66e314deb9ff
LucasJQ13 <167819372+LucasJQ13@users.noreply.github.com>
2026-06-13 12:51:30 -0300

    Move dialog text into dedicated module

---
+++ src/data/dialogs.js
@@ -0,0 +1,24 @@
+export const dialogs = {
+  elderBeforeCrystal: [
+    'Anciano Aldeano',
+    'El bosque está raro...',
+    'Busca el cristal celeste al sudeste.',
+    'Con él podremos abrir el viejo sendero.'
+  ],
+  elderAfterCrystal: [
+    'Anciano Aldeano',
+    '¡Lo encontraste!',
+    'Esta aldea vuelve a tener esperanza.',
+    'Próximo objetivo: abrir el sendero del norte.'
+  ],
+  crystalFound: [
+    'Cristal Celeste',
+    'Obtuviste el primer objeto clave.',
+    'Vuelve con el anciano.'
+  ],
+  signNorth: [
+    'Cartel',
+    'Bosque Viejo al norte.',
+    'Ruinas del Cristal al este.'
+  ]
+};
