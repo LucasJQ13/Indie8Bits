d976e6180bbf62914724c5fe011bd73190063032
LucasJQ13 <167819372+LucasJQ13@users.noreply.github.com>
2026-06-13 14:45:28 -0300

    Add decorative prop data

---
+++ src/data/props.js
@@ -0,0 +1,10 @@
+export const props = [
+  { id: 'campfire', type: 'campfire', x: 214, y: 154, w: 18, h: 18, solid: true },
+  { id: 'well', type: 'well', x: 356, y: 176, w: 28, h: 22, solid: true },
+  { id: 'stoneA', type: 'stone', x: 118, y: 158, w: 14, h: 10, solid: true },
+  { id: 'stoneB', type: 'stone', x: 390, y: 84, w: 14, h: 10, solid: true },
+  { id: 'grassA', type: 'grassTuft', x: 62, y: 92, w: 12, h: 10, solid: false },
+  { id: 'grassB', type: 'grassTuft', x: 302, y: 72, w: 12, h: 10, solid: false },
+  { id: 'grassC', type: 'grassTuft', x: 438, y: 220, w: 12, h: 10, solid: false },
+  { id: 'crateA', type: 'crate', x: 292, y: 178, w: 16, h: 16, solid: true }
+];
