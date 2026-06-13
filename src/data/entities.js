0d290168e758b7506d392ea847d1ecbf1abcffb7
LucasJQ13 <167819372+LucasJQ13@users.noreply.github.com>
2026-06-13 12:51:25 -0300

    Move entity data into dedicated module

---
+++ src/data/entities.js
@@ -0,0 +1,38 @@
+export const playerStart = {
+  x: 80,
+  y: 80,
+  w: 12,
+  h: 14,
+  speed: 72,
+  facing: 'down',
+  step: 0
+};
+
+export const elder = {
+  id: 'elder',
+  x: 252,
+  y: 126,
+  w: 12,
+  h: 14,
+  name: 'Anciano Aldeano',
+  palette: { body: '#6d28d9', face: '#f1c27d', hair: '#f8fafc' }
+};
+
+export const crystal = {
+  id: 'skyCrystal',
+  x: 414,
+  y: 260,
+  w: 10,
+  h: 10
+};
+
+export const signs = [
+  {
+    id: 'northSign',
+    x: 178,
+    y: 226,
+    w: 12,
+    h: 12,
+    dialog: 'signNorth'
+  }
+];
